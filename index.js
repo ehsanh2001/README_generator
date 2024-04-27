1127f2062f0c8289de640fefb2546113e86f42a7
ehsanh2001 <ehsanh2001@gmail.com>
2024-04-27 12:54:34 -0400

    Create the list of questions

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide usage information.",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide contribution guidelines.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide test instructions.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license.",
+        choices: ["MIT", "GNU GPLv3", "Apache 2.0", "ISC", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address.",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message:
+            "Please provide instructions for how to reach you with questions.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+}
 
 // Function call to initialize app
 init();
