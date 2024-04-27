f1390cb083eb97c69ec13b47ef4993c5ba0dd1a4
ehsanh2001 <ehsanh2001@gmail.com>
2024-04-27 13:38:07 -0400

    Implemet generateMarkdown()

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,59 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+  
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} license.
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
 
 `;
 }
